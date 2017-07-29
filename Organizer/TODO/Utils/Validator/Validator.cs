a793a42d2c44dd213fa6b2116d2a55034af83487
CecoM <ceco_milchev@abv.bg>
2017-07-29 15:02:37 +0300

    Added Validator, Constants, implemented username

--- Organizer/TODO/Utils/Validator/Validator.cs
+++ Organizer/TODO/Utils/Validator/Validator.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TODO.Utils.GlobalConstants;
 
 namespace TODO.Utils.Validator
 {
-    class Validator
+    public class Validator
     {
+        #region Methods
+        public static void CheckUserName (string input)
+        {
+            Regex rgx = new Regex(Constants.RegexNamePattern);
+
+            if (!rgx.IsMatch(input))
+            {
+                throw new ArgumentException($"{input} is not a valid name. Use only letters, numbers and underscore.");
+            }
+        }
+        public static void CannotBeNull(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException("Input cannot be null or empty.");
+            }
+        }
+        public static void CheckNameLength(string input, int min)
+        {
+            if (input.Length < min)
+            {
+                throw new ArgumentException($"Name cannot be less than {min} characters");
+            }
+        }
+        #endregion
     }
 }
