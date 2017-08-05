8eb43239c5d01e580442538066654faa733f9ed7
SvetozarMateev <vidic9898@gmail.com>
2017-08-05 23:23:03 +0300

    Attempt to fix Messages.cs

---
+++ Organizer/TODO/Utils/GlobalConstants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO.Utils.GlobalConstants
+{
+    public static class Messages
+    {
+        public static string NotebookCreated(string notebookName)
+        {
+            return $"Notebook \"{notebookName}\" added successfully !";
+        }
+
+        public static string NoteCreated(string title)
+        {
+            return $"Note {title} added successfully !";
+        }
+    }
+}
